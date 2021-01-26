49a6ca473b53a9d893331240104050e2ec1822ef
Lucheng <Lucheng@wondertek.com.cn>
2021-01-26 09:18:01 +0800

    axios 取消请求测试

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,102 +1,61 @@
 <template>
-  <div class="box">
-    <canvas id="canvas" width="300" height="200"></canvas>
+  <div class="page" id="app">
+    <button @click="getMsg" class="get-msg">获取数据</button>
+    <button @click="cancelGetMsg" class="cancel">取消获取</button>
+    <ul>
+      <li v-for="(item,index) in items" :key="index">{{ item.name }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
+import { Chrome } from "vue-color";
+import axios from 'axios'
 export default {
   name: "HelloWorld",
   props: {
     msg: String,
   },
+  components: {},
   data() {
     return {
-      
+      message: "Hello Vue!",
+      items: [],
+      cancel: null,
     };
   },
-
-  mounted() {
-    var canvas = new fabric.Canvas('canvas');
-    var rect = new fabric.Rect({
-        top : 50,
-        left : 100,
-        width : 100,
-        height : 70,
-        fill : 'red'
-    });
-    canvas.add(rect);
-  },
-  computed: {
-    
-  },
-  watch: {
-    
-  },
+  watch: {},
   methods: {
-  
+    getMsg () {
+        let CancelToken = axios.CancelToken
+        let self = this
+        axios.get('http://jsonplaceholder.typicode.com/comments', {
+          cancelToken: new CancelToken(function executor(c) {
+            self.cancel = c
+            console.log(c)
+            // 这个参数 c 就是CancelToken构造函数里面自带的取消请求的函数，这里把该函数当参数用
+          })
+        }).then(res => {
+          this.items = res.data
+        }).catch(err => {
+          console.log(err)
+        })
+
+
+        //手速够快就不用写这个定时器了，点击取消获取就可以看到效果了
+        // setTimeout(function () {
+        //   //只要我们去调用了这个cancel()方法，没有完成请求的接口便会停止请求
+        //   self.cancel()
+        // }, 100)
+      },
+      //cancelGetMsg 方法跟上面的setTimeout函数是一样的效果，因为手速不够快，哦不，是因为网速太快，导致我来不及点取消获取按钮，数据就获取成功了
+      cancelGetMsg () {
+        // 在这里去判断你的id 1 2 3，你默认是展示的tab1，点击的时候不管你上一个请求有没有执行完都去调用这个cancel()，
+        this.cancel()
+      }
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-.canvas {
-}
-
-/* .video-control {
-    height: 50px;
-    width: 100%;
-    box-sizing: border-box;
-    padding: 10px;
-    color: #fff;
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-    font-size: 14px;
-    .timing {
-      flex: 2;
-    }
-    .control-controller {
-      flex: 1;
-      .is-video-play-icon {
-        padding: 0 10px;
-      }
-    }
-    .control-slider {
-      flex: 4;
-    }
-    .control-voice {
-      box-sizing: border-box;
-      flex: 1;
-      display: flex;
-      align-items: center;
-      justify-content: space-between;
-      padding: 0 10px;
-      .voice-slider {
-        width: 70%;
-        /deep/ .el-slider{
-          .el-slider__runway{
-            height:3px;
-            width:85%;
-            .el-slider__bar{
-              height:3px;
-            }
-            .el-slider__button-wrapper{
-              top:-16px;
-              .el-slider__button{
-                height:8px;
-                width:8px;
-              }
-            }
-          }
-        }
-      }
-    }
-    .icon-class {
-      font-size: 16px;
-      color: #fff;
-      cursor: pointer;
-    }
-  } */
-</style>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -4,16 +4,21 @@ import App from './App.vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
+import axios from 'axios'
 import './icons'
 
 import {fabric} from 'fabric'
+import { Chrome} from 'vue-color'
 
 Vue.use(fabric)
-
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  components: {
+    'chrome-picker':Chrome
+  }
+  
 }).$mount('#app')
